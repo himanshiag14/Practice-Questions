4afafae3d90aed2deba65e13bcf2ef8835376c82
himanshiag14 <tishaag14@gmail.com>
2025-12-30 18:23:59 +0530

    Adding more Solution

---
+++ Problem02/solution_03.py
@@ -0,0 +1,6 @@
+#Multipication  table Printer
+
+num = int(input("Enter the Table Number : "))
+
+for i in range(1,11):
+    print(num, " X ", i ," = ",num*i)
---
+++ Problem02/solution_04.py
@@ -0,0 +1,9 @@
+#reverse a String
+
+st = input("Enter a String")
+rev_str =""
+
+for i in st:
+    rev_str = i +rev_str
+print(rev_str)
+   
